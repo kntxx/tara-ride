2fac6d7884c6de33bab72df8bc48f0f41bf83a10
kentatixx <kentatixx@gmail.com>
2025-11-24 17:15:18 +0800

    Fix CORS for Vercel deployment

--- server/server.js
+++ server/server.js
@@ -24,6 +24,7 @@ const io = new Server(server, {
   cors: {
     origin: process.env.CLIENT_URL || "http://localhost:5173",
     methods: ["GET", "POST"],
+    credentials: true,
   },
 });
 
@@ -46,7 +47,12 @@ io.on("connection", (socket) => {
 app.set("io", io);
 
 // Middleware
-app.use(cors());
+app.use(
+  cors({
+    origin: process.env.CLIENT_URL || "http://localhost:5173",
+    credentials: true,
+  })
+);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
